361abce29f618acfe2646376b3224adaa813cf8a
Stohelitskii <Andrenord282@gmail.com>
2023-12-03 18:14:41 +0500

    правка стилей

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import cvPdf from "./assets/pdf/my-cv.pdf";
 const App = () => {
     return (
         <div>
-            <iframe src={cvPdf} style={{ minWidth: "100vw", minHeight: "100vh" }}></iframe>
+            <iframe src={cvPdf} style={{ width: "100%", minHeight: "100vh" }}></iframe>
         </div>
     );
 };
